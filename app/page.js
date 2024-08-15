6a78239b697502d6392f3457dfd7d723c594dcbc
Abdulqadir Abdillahi <abdiqaadirabdillahi@gmail.com>
2024-08-15 18:16:35 +0100

    linked pages with a button

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import HeaderImagePage from "@/public/Image/HeaderImage/HeaderImage";
 // import HowItWorks from "@/public/Image/HowItWorks/HowItWorksImage";
 import HowItWorksImage from "@/public/Image/howitworks/howitworks";
 import HowItWorksSection from "./src/components/HowItWorks/how-it-works";
+import Link from "next/Link";
 
 import { Aleo } from "next/font/google";
 const aleo = Aleo({ subsets: ["latin"] });
@@ -11,6 +12,7 @@ const aleo = Aleo({ subsets: ["latin"] });
 export default function Home() {
   return (
     <div className={aleo.className}>
+      <Link href="founders">This is the founders page</Link>
       <HeaderImagePage />
       <Hero />
       <HowItWorksImage />
